d684e2ebf2b23ec01274cd4b410302e81a37db8c
Vu Huynh <vqhuynh3@gmail.com>
2020-08-25 00:33:44 -0700

    Add email subscription form

--- client/src/components/Subscribe.jsx
+++ client/src/components/Subscribe.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class Subscribe extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value})
+  }
+
+  render() {
     return (
       <div className="subscribe-container">
         <h1>Subscribe for Updates</h1>
-        
+        <form>
+          <label>
+            Email:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          {/* <input>Submit</input> */}
+        </form>
       </div>
     )
   }
