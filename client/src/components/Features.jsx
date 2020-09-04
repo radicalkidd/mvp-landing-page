616080333d914c45e7f5cd65625bb8a693128bd7
Vu Huynh <vqhuynh3@gmail.com>
2020-09-03 23:18:00 -0700

    gradient styled feature boxes

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -9,8 +9,8 @@ class Features extends Component {
       <div className="features-container">
         {/* <div className="feature-content"> */}
           <div className="f1 box-1">1</div>
-          <div className="f2">2</div>
-          <div className="f3">3</div>
+          <div className="f2 box-2">2</div>
+          <div className="f3 box-3">3</div>
           {/* <ul className="features-list">
             <li className="feature-item"><p>The standard Lorem Ipsum passage, used since the 1500s
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></li>
