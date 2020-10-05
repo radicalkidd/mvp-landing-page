81a49e3f5c742fa8bcba1283b8e05d1f0e953015
Vu Huynh <vqhuynh3@gmail.com>
2020-10-04 20:56:45 -0700

    update Features and CSS

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { features } from 'process';
 
 class Features extends Component {
   render () {
@@ -10,11 +9,21 @@ class Features extends Component {
           <div className="f1 box-1">
             <div>
             <h4>Support</h4>
-            <p>24/7 Support</p>
+            <p>24/7 Support from our experienced staff</p>
             </div>
           </div>
-          <div className="f2 box-2">2</div>
-          <div className="f3 box-3">3</div>
+          <div className="f2 box-2">
+          <div>
+            <h4>Monitors</h4>
+            <p>Monitoring over 200+ Shopify sites and Footsites</p>
+            </div>
+          </div>
+          <div className="f3 box-3">
+          <div>
+            <h4>Free ACO Slots</h4>
+            <p>For our premium members, we provide free ACO slots!</p>
+            </div>
+          </div>
       </div>
 
       </div>
