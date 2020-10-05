5fe610469f0514da1d5325b1b0c50ddad50db87c
Vu Huynh <vqhuynh3@gmail.com>
2020-10-05 15:59:19 -0700

    Handle Multiple Inputs for Subscribe Component

--- client/src/components/Subscribe.jsx
+++ client/src/components/Subscribe.jsx
@@ -4,6 +4,7 @@ class Subscribe extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
       email: ''
     };
     this.handleChange = this.handleChange.bind(this);
@@ -11,7 +12,7 @@ class Subscribe extends Component {
   }
 
   handleChange(e) {
-    this.setState({value: e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
@@ -25,8 +26,12 @@ class Subscribe extends Component {
         <h1>Subscribe for Updates</h1>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Email:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <h2>Name: </h2>
+            <input name="fullName" type="text" onChange={this.handleChange} placeholder="Vu Huynh"/>
+          </label>
+          <label>
+            <h2>Email: </h2>
+            <input name="email" type="text" onChange={this.handleChange} placeholder="vu.huynh@gmail.com"/>
           </label>
             <button>Submit</button>
         </form>
